c32e81b95a0f9a5798a452a4f365f0272becdd25
bekzodfronten <bs3328545@gmail.com>
2025-12-03 18:06:27 +0500

    push

---
+++ js/thankYou.js
@@ -0,0 +1,36 @@
+const SHEET_URL =
+  "https://script.google.com/macros/s/AKfycby8NhAistIsWsWewxTsxv4p_e54ORNfRPJxrz3WCWsdS4nISbPgm5xQDrzv7ma2-x8cCw/exec";
+
+async function sendFormData() {
+  const o = localStorage.getItem("FormData");
+  if (!o) return void console.log("Ma'lumotlar yo‘q");
+
+  const t = JSON.parse(o);
+
+  // Google Sheet uchun FormData
+  const a = new FormData();
+  a.append("Ism", t.name);
+  a.append("Telefon raqam", t.phone);
+  a.append("Royhatdan o'tgan vaqti", t.time);
+
+  try {
+    // Ikkalasi parallel yuboriladi
+    const [sheetRes] = await Promise.all([
+      fetch(SHEET_URL, { method: "POST", body: a }),
+    ]);
+
+    // Har ikkala so‘rov ham muvaffaqiyatli bo‘lishini tekshiramiz
+    if (!sheetRes.ok) throw new Error("Google Sheet API response was not ok");
+
+    // Muvaffaqiyatli bo‘lsa → localStorage tozalanadi
+    console.log("Ma'lumotlar muvaffaqiyatli yuborildi ✅");
+
+  } catch (err) {
+    console.error("Form yuborishda xatolik:", err);
+    document.getElementById("errorMessage").style.display = "block";
+  }
+}
+
+window.onload = () => {
+  sendFormData();
+};
